5043566f25b78351eae45a9642534c8ea7c6e09b
ht <Illioa@users.noreply.huggingface.co>
2023-11-05 16:11:21 +0000

    Update app.py

--- app.py
+++ app.py
@@ -140,7 +140,7 @@ with st.status("Checking requirements..."):
 
 def fetch_data(selected_category):
     with st.status("Running Parse..."):
-        result_df = asyncio.get_event_loop().run_until_complete(get_channels_by_categoryselected_category))
+        result_df = asyncio.get_event_loop().run_until_complete(get_channels_by_categoryselected_category)
     st.dataframe(result_df)
 
 if __name__ == "__main__":
