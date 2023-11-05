3bc84035280e9b65cce40d116b8270dc70142d39
ht <Illioa@users.noreply.huggingface.co>
2023-11-05 15:52:00 +0000

    Update app.py

--- app.py
+++ app.py
@@ -6,15 +6,6 @@ from requests_html import AsyncHTMLSession
 import streamlit as st
 import threading
 
-def safe_eval(expression):
-
-
-    try:
-       return round(eval(expression),2)
-    except Exception as e:
-        print(f"Error evaluating expression '{expression}': {e}")
-        return None
-
 
 async def get_dict_topic():
     asession = AsyncHTMLSession()
@@ -35,20 +26,22 @@ async def get_dict_topic():
 
 async def get_channels_by_category(category_name):
     asession = AsyncHTMLSession()
+    
     with open('categories.json', 'r') as f:
         categoriesDict = eval(f.read())
     category_link = categoriesDict[category_name]
 
-    response = await asession.get(category_link)
+    logging.info(f'Fetching data from {category_link}')
+
+    response = await asession.get(category_link, headers=headers)
     channels_list = response.html.find("#category-list-form > div.row.justify-content-center.lm-list-container > div")
     channels_data = []
-    for channel in channels_list:
+    for channel in tqdm(channels_list, desc=f'Fetching Channels for {category_name}', ncols=100):
         channel_name = channel.find('div.font-16.text-dark.text-truncate', first=True).text
-        st.write(f"Scanning {channel_name}...")
         Tgstat_link = str(list(channel.absolute_links)[0]) +'/stat'
 
         async def get_values_by_channel(stats_link):
-            r = await asession.get(stats_link)
+            r = await asession.get(stats_link, headers=headers)
             r = r.html
             subscribers_count = int(r.find("#sticky-center-column > div > div > div:nth-child(1) > div > h2", first=True).text.replace(' ', ''))
 
@@ -60,15 +53,19 @@ async def get_channels_by_category(category_name):
             required_columns = ['Подписчики', 'Индекс цитирования','Средний охват 1 публикации','Средний рекламный охват 1 публикации','Возраст канала']
             for key in dict_.keys():
               if key not in required_columns:
-                transformed_value = safe_eval(dict_[key][0].replace(' ', '').replace('всего',' ').replace('%','/100').replace('k','*1000'))
-                if transformed_value is not None:
-                    dict_[key] = transformed_value
+                try:
+                  dict_[key]=eval(dict_[key][0].replace(' ', '').replace('всего',' ').replace('%','/100').replace('k','*1000'))
+                except:
+                  pass
 
             def process_metric(metric, keys, transformations, mainKey):
                 for i in range(1, len(metric)):
                     if metric[i] in keys:
                         key = metric[i].capitalize()
-                        value = safe_eval(metric[i - 1].replace(' ', '').replace('%', '/100').replace('k', '*1000')) if metric[i] != 'канал создан' else metric[i - 1]
+                        try:
+                          value = round(eval(metric[i - 1].replace(' ', '').replace('%', '/100').replace('k', '*1000')),2) if metric[i] != 'канал создан' else metric[i - 1]
+                        except:
+                          value = None
                         transformations[f'{mainKey} ({key})'] = value
 
             keys_and_transformations = {
@@ -80,54 +77,54 @@ async def get_channels_by_category(category_name):
             }
 
             for key, values in keys_and_transformations.items():
-                process_metric(dict_[key], values, dict_, key)
+                process_metric(dict_[key], values, dict_,key)
+
 
             for key in dict_.keys():
-                if key in required_columns and key != 'Возраст канала':
-                    transformed_value = safe_eval(dict_[key][0].replace(' ', '').replace('всего',' ').replace('%','/100').replace('k','*1000'))
-                    if transformed_value is not None:
-                        dict_[key] = transformed_value
-                        
+              if key in required_columns and key != 'Возраст канала':
+                try:
+                  dict_[key]=eval(dict_[key][0].replace(' ', '').replace('всего',' ').replace('%','/100').replace('k','*1000'))
+                except:
+                  pass
             del dict_['Возраст канала']
             dict_['TG Link'] = list(r.find('body > div.wrapper > div > div.content.p-0.col > div.container-fluid.px-2.px-md-3 > div:nth-child(2) > div > div > div > div.col-12.col-sm-7.col-md-8.col-lg-6 > div.text-center.text-sm-left > a')[0].absolute_links)[0]
             return dict_
 
+
         subscribers_count = int(channel.find('div.font-12.text-truncate', first=True).text.replace(' подписчиков', '').replace(' ', ''))
         if subscribers_count > 10000:
-            values = await get_values_by_channel(Tgstat_link)
-            values['Name'] = channel_name
-            values['Tgstat_link'] = Tgstat_link
+          values = await get_values_by_channel(Tgstat_link)
+          values['Name'] = channel_name
+          values['Tgstat_link'] = Tgstat_link
 
-            channels_data.append(values)
+          channels_data.append(values)
     df = pd.DataFrame(channels_data)
     df = df.rename(columns={'Возраст канала (Канал создан)': 'Канал создан'})
-    try:
-        df['Канал создан'] = pd.to_datetime(df['Канал создан'], format='%d.%m.%Y', errors='coerce')
-    except:
-        pass
+    # df['Канал создан'] = pd.to_datetime(df['Канал создан'], format='%d.%m.%Y', errors='coerce')
     desired_column_order = [
-        'Name', 'TG Link', 'Tgstat_link','Канал создан',
-        'Подписчики', 'Индекс цитирования', 'Средний охват 1 публикации',
-        'Средний рекламный охват 1 публикации', 'Публикации',
-        'Вовлеченность подписчиков (err)', 'Вовлеченность подписчиков (er)',
-        'Подписчики (Сегодня)', 'Подписчики (За неделю)',
-        'Подписчики (За месяц)', 'Индекс цитирования (Уп. каналов)',
-        'Индекс цитирования (Упоминаний)', 'Индекс цитирования (Репостов)',
-        'Средний охват 1 публикации (Err)',
-        'Средний охват 1 публикации (Err24)',
-        'Средний рекламный охват 1 публикации (За 12 часов)',
-        'Средний рекламный охват 1 публикации (За 24 часа)',
-        'Средний рекламный охват 1 публикации (За 48 часов)',
-        'Подписки/отписки за 24 часа',
-        'Пол подписчиков', 'Stories'
+      'Name', 'TG Link', 'Tgstat_link','Канал создан',
+      'Подписчики', 'Индекс цитирования', 'Средний охват 1 публикации',
+      'Средний рекламный охват 1 публикации', 'Публикации',
+      'Вовлеченность подписчиков (err)', 'Вовлеченность подписчиков (er)',
+      'Подписчики (Сегодня)', 'Подписчики (За неделю)',
+      'Подписчики (За месяц)', 'Индекс цитирования (Уп. каналов)',
+      'Индекс цитирования (Упоминаний)', 'Индекс цитирования (Репостов)',
+      'Средний охват 1 публикации (Err)',
+      'Средний охват 1 публикации (Err24)',
+      'Средний рекламный охват 1 публикации (За 12 часов)',
+      'Средний рекламный охват 1 публикации (За 24 часа)',
+      'Средний рекламный охват 1 публикации (За 48 часов)',
+       'Подписки/отписки за 24 часа',
+      'Пол подписчиков', 'Stories'
     ]
-
+  
     # Reorder the columns in the DataFrame
     df = df[desired_column_order]
-
+  
     # Save the DataFrame to CSV file
     df.to_csv(f'{category_name}.csv', index=False)
     return df
+    
 with st.status("Checking requirements..."):
     if not os.path.exists('categories.json'):
         
